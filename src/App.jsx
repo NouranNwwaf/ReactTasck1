cffa5580b8aae9eacb4705f5324b97530e713daa
unknown <masalmanouran@gmail.com>
2024-08-25 20:22:14 +0300

    up to github

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Clients from "./assets/Clients";
+import Employees from "./assets/Employees";
+import Footer from "./assets/Footer";
+import Navbar from "./assets/Navbar";
+
+export default function App()
+{
+  return <dev>
+    <Navbar/>
+    <Employees/>
+    <Clients/>
+    <Footer/>
+  </dev>
+}
---
+++ src/assets/Navbar.jsx
@@ -0,0 +1,6 @@
+export default function Navbar()
+{
+    return <nav>
+        <a>facebook</a>
+    </nav>
+}
